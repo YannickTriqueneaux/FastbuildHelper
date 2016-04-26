50936ea03624c750677784ebed858fd0b552154f
Denis Berghe <DenisBerghe@hq-mtl.opal-rt.com>
2016-04-26 17:17:10 -0400

    Improved VisualStudio utilisation. Degraded linux use. - X86 configuration renamed to Win32 - Merge of the different configuration in Visual. Now only Debug/Release for the configuration, and only Win32/x86 in the platform - Fixed a bug where properties were not found for platform other than x64

--- Examples/HelloWorld/HelloWorld.cc
+++ Examples/HelloWorld/HelloWorld.cc
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <stdlib.h>
+#undef NDEBUG
+#include <cassert>
 
 int main(int argc, char** argv) {
 
 	std::cout << "Hello World !" << std::endl;
+
+	int toto;
+	//scanf("%d", &toto);
+
+	int *p = 0;
+	//*p = 2;
+
+	assert(false);
 	
 	return 0;
 }
